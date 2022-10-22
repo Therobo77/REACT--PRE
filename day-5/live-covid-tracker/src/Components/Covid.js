10beedb911f4d62ba9740d009ad4a97aa47926f2
Therobo77 <anshusinha8873@gmail.com>
2022-10-22 19:38:06 +0530

    done

--- day-5/live-covid-tracker/src/Components/Covid.js
+++ day-5/live-covid-tracker/src/Components/Covid.js
@@ -3,12 +3,8 @@ import React from 'react'
 const Covid = () => {
   return (
     <div>  
-       <div>
-       <div>
         <h1> 😡 LIVE</h1>
         <h2>COVID -19 CORONAVIRUS TRACKER</h2>
-        </div>
-        </div>
     </div>
   )
 }
