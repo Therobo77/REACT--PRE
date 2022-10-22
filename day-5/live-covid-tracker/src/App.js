519276ff803b56c9f6b11af5539f77b5cb335eaa
Therobo77 <anshusinha8873@gmail.com>
2022-10-22 07:21:01 +0530

    live covid tracker

---
+++ day-5/live-covid-tracker/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+import Covid from './Components/Covid'
+
+const App = () => {
+  return (
+    <>
+      <Covid/>
+    </>
+  )
+}
+
+export default App
