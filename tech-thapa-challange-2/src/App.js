71899c85dc6c84b7e6b16688f5a726a95f19c81a
Therobo77 <anshusinha8873@gmail.com>
2022-11-12 05:10:46 +0530

    ok

---
+++ tech-thapa-challange-2/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Clalange2 from './Components/Clalange2';
+
+function App() {
+  return (
+    <div className="App">
+      <Clalange2/>
+    </div>
+  );
+}
+
+export default App;
---
+++ tech-thapa-challange-2/src/Components/Clalange2.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Clalange2 = () => {
+    const Name = `Vikash`
+    const CrDate = new Date().toLocaleDateString();
+    const CrTime = new Date().toLocaleTimeString();
+  return (
+    <div>
+        <h1>Hey My Name is {Name}</h1>
+        <p>Current Date {CrDate}</p>
+        <p>Current Time {CrTime}</p>
+    </div>
+  )
+}
+
+export default Clalange2
