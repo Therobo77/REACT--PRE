20d299452c54829394e075276f7575060a64c2c4
Therobo77 <anshusinha8873@gmail.com>
2022-11-12 06:58:16 +0530

    ok

--- tech-thapa-challange-2/src/Components/ContentEditable.jsx
+++ tech-thapa-challange-2/src/Components/ContentEditable.jsx
@@ -2,9 +2,18 @@ import React from 'react'
 
 const ContentEditable = () => {
     const name = `Vikash Kumar Lal`;
+    const img1 = "https://avatars.githubusercontent.com/u/105917542?v=4"
+    const img2 = "https://avatars.githubusercontent.com/u/105917542?v=4"
+    const img3 = "https://avatars.githubusercontent.com/u/105917542?v=4"
+    const googleLink="https://www.youtube.com/watch?v=OloAP8p1k1w&list=PLwGdqUZWnOp3aROg4wypcRhZqJG3ajZWJ&index=14"
   return (
     <>
         <h1 contentEditable={true}>My Name is {name}</h1>
+        <img src={img1} alt="Vikash"/>
+        <img src={img2} alt="Vikash"/>
+        <a href={googleLink} target="_aa">
+        <img src={img3} alt="Vikash"/>
+        </a>
     </>
   )
 }
